f985c4c229479a59618dcbb7d973deb9d028d8c8
Shubham soni <ss4185667@gmail.com>
2023-11-20 11:28:54 +0530

    some changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,38 +3,92 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-      <Link to="/" className="navbar-brand text-light">
-        PODSTAR
-      </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div className="navbar-nav">
-          <Link to="/" className="nav-item nav-link active text-light">
-            Dashboard
-          </Link>
-          <Link to="/all" className="nav-item nav-link text-light">
-            All
-          </Link>
-          <Link to="/upload" className="nav-item nav-link text-light">
-            Upload
-          </Link>
-          <Link to="/login" className="nav-item nav-link text-light">
-            Login
-          </Link>
+    <div style={{ backgroundColor: "#343a40" }}>
+      <nav class="navbar navbar-expand-lg bg-dark px-5">
+        <Link to="/" className="navbar-brand">
+          <span className="logo-text text-light fw-bold">PODSTAR</span>
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-item nav-link active" href="/">
+              <Link to="/" className="nav-link text-light">
+                Dashboard
+              </Link>
+            </a>
+            <a class="nav-item nav-link" href="/all">
+              <Link to="/all" className="nav-link text-light">
+                All
+              </Link>
+            </a>
+            <a class="nav-item nav-link" href="/upload">
+              <Link to="/upload" className="nav-link text-light">
+                Upload
+              </Link>
+            </a>
+            <a class="nav-item nav-link" href="/login">
+              <Link to="/login" className="nav-link text-light">
+                Login
+              </Link>
+            </a>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+
+      {/* <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <Link to="/" className="navbar-brand">
+            <span className="logo-text text-light">PODSTAR</span>
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to="/" className="nav-link text-light">
+                  Dashboard
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/all" className="nav-link text-light">
+                  All
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/upload" className="nav-link text-light">
+                  Upload
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to="/login" className="nav-link text-light">
+                  Login
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav> */}
+    </div>
   );
 };
 
